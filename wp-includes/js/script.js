98aaf6c185cb9215409bba3a806e52b9d0e361b6
Md Tanzim Hossain <tanzim.7400@gmail.com>
2025-07-19 11:26:28 +0200

    Update script.js

--- wp-includes/js/script.js
+++ wp-includes/js/script.js
@@ -1,1 +1,8 @@
-
+function sendEmail() {
+    let params = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,
+        message : document.getElementById('message').value
+    }
+    emailjs.send('service_aaq7c88', 'template_5iwcesf', params).then(alert('Message Sent Successfully!!!'));
+}
